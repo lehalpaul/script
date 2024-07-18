2650b73a69b0602340e4fbd2e217d3260d5b561f
lehalpaul <152332655+lehalpaul@users.noreply.github.com>
2024-07-17 21:35:32 -0700

    Update script_main.py

--- script_main.py
+++ script_main.py
@@ -59,11 +59,11 @@ You are an AI assistant tasked with creating phone scripts for sales representat
 Task:
 Generate a phone script for an inbound lead. Use the following information to craft a friendly and professional script that includes necessary sections like greeting, vehicle information, and closing statements.
  Lead Information is  given below ,you need to generate script based on question asked about particular lead.The Phone given in data is Customer Phone.
-Lead Information:
+ Lead Information:
 Prospect Status: Interested
 Vehicles of Interest:
-Interest: Buy, Status: Used, Year: 2015, Make: Ford, Model: Freestar , VIN: ,Trim:
-Interest: Trade-in, Status: New, Year: 2006, Make: Chevrolet, Model: Uplander ,VIN ,Trim:
+Interest: Buy, Status: Used, Year: 2011, Make: Buick, Model: Enclave, Trim: Premium, VIN: FVVH8TCYMVWB57M0E
+Interest: Trade-in, Status: New, Year:,Make: Kia ,Model: ,Trim: , VIN:
 Customer Details:
 First Name: Guilian
 Last Name: MINION
@@ -76,7 +76,8 @@ ID: 1
 Source: Unknown
 Lead Inventory Items:
 Inventory: ""
-Returning Customer:True
+Returning Customer: True
+
 
 
 
